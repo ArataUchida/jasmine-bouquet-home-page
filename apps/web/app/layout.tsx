38fdd61729a2bc5eb85acc38c315cf8dd91d6c63
ArataUchida <neitianx273@gmail.com>
2026-09-13 23:10:04 +0900

    Top画像

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,8 +8,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ja">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="m-0 p-0">
+        {children}
+      </body>
     </html>
   );
 }
