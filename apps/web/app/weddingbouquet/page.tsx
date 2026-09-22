84fbe8a03a827f77b01952ecdf46d025d984abd4
ArataUchida <neitianx273@gmail.com>
2026-09-22 13:17:34 +0900

    wedingbouquetページの親コンポーネント

---
+++ apps/web/app/weddingbouquet/page.tsx
@@ -0,0 +1,31 @@
+import Pagination from "../components/product/Pagination";
+import ProductCard from "../components/product/ProductCard";
+import { SlidersHorizontal } from "lucide-react";
+import { getWeddingBouquets } from "../services/weddingBouquets";
+import CommonProductPage from "../components/product/CommonProductPage";
+
+export default async function WeddingBouquetPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const weddingBouquets = await getWeddingBouquets();
+  const { page } = await searchParams;
+  const currentPage = Number(page) || 1;
+  const productsPerPage = 12;
+  const totalPages = Math.ceil(weddingBouquets.length / productsPerPage);
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const currentProducts = weddingBouquets.slice(
+    startIndex,
+    startIndex + productsPerPage,
+  );
+
+  return (
+    <CommonProductPage
+      titleImage="/products/weddingbouquet_header.PNG"
+      products={currentProducts}
+      currentPage={currentPage}
+      totalPages={totalPages}
+    />
+  );
+}
